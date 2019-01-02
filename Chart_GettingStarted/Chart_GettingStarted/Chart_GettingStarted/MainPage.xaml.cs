921b0520e75a1663fa6541d39339ff5d4d1da488
ManivannanE <manivannanelangovan@syncfusion.com>
2019-01-02 11:07:43 +0530

    Sample and description added for chart getting started.

---
+++ Chart_GettingStarted/Chart_GettingStarted/Chart_GettingStarted/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Chart_GettingStarted
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+
+    public class ViewModel
+    {
+        public List<Person> Data { get; set; }
+        public ViewModel()
+        {
+            Data = new List<Person>()
+            {
+                new Person("David", 180),
+                new Person("Micheal", 170),
+                new Person("Steve", 160),
+                new Person("Joel", 182)
+            };
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public double Height { get; set; }
+
+        public Person(string name, double height)
+        {
+            this.Name = name;
+            this.Height = height;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Getting Started for Xamarin.Forms Charts
 This is demo application of Xamarin.Forms SfChart control. The minimal set of required properties have been configured in this project to get started with SfChart in Xamarin.Forms.
+
+For more details please refer the Xamarin.Forms UG documentation [Getting Started](https://help.syncfusion.com/xamarin/sfchart/getting-started) link.
+
+## <a name="requirements-to-run-the-demo"></a>Requirements to run the demo ##
+
+* [Visual Studio 2017](https://visualstudio.microsoft.com/downloads/) or [Visual Studio for Mac](https://visualstudio.microsoft.com/vs/mac/).
+* Xamarin add-ons for Visual Studio (available via the Visual Studio installer).
+
+## <a name="troubleshooting"></a>Troubleshooting ##
+### Path too long exception
+If you are facing path too long exception when building this example project, close Visual Studio and rename the repository to short and build the project.
